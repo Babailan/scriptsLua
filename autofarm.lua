777587e9e514accc7b57acd5e76ffdce540914da
Ronnel <83863770+Babailan@users.noreply.github.com>
2022-05-09 12:37:24 +0800

    Create autofarm.lua

---
+++ autofarm.lua
@@ -0,0 +1,6 @@
+_G.Color = Color3.fromRGB(52, 190, 255)
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hajibeza/RIPPERMAIN/main/README.md"))()
+else
+    print("Wrong Map")
+end
