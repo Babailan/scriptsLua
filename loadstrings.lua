537b6c00fba6106be5f7fbd45f42e52fcbbe7531
Ronnel <83863770+Babailan@users.noreply.github.com>
2022-05-11 02:04:55 +0800

    Create loadstrings.lua

---
+++ loadstrings.lua
@@ -0,0 +1,17 @@
+-- autofarm
+-- _G.Color = Color3.fromRGB(52, 190, 255)
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/hajibeza/RIPPER-HUB/main/NEWBF.lua"))()
+
+-- other
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/main/ATR",true))()
+
+--second script aimbot
+
+--loadstring(game:HttpGet("https://raw.githubusercontent.comAveriias/Universal-SilentAim/main/main.lua"))()
+ 
+
+--server hoop 
+
+-- local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
+
+-- module:Teleport(game.PlaceId)
