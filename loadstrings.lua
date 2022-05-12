9253dcfc0e70b3dc6c6967f0115042c8d2455721
Ronnel <83863770+Babailan@users.noreply.github.com>
2022-05-12 16:56:32 +0800

    Update loadstrings.lua

--- loadstrings.lua
+++ loadstrings.lua
@@ -1,17 +1,17 @@
 -- autofarm
--- _G.Color = Color3.fromRGB(52, 190, 255)
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/hajibeza/RIPPER-HUB/main/NEWBF.lua"))()
+_G.Color = Color3.fromRGB(52, 190, 255)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hajibeza/RIPPER-HUB/main/NEWBF.lua"))()
 
 -- other
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/main/ATR",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/main/ATR",true))()
 
 --second script aimbot
 
---loadstring(game:HttpGet("https://raw.githubusercontent.com/Averiias/Universal-SilentAim/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Averiias/Universal-SilentAim/main/main.lua"))()
  
 
 --server hoop 
 
--- local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
+local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
 
 -- module:Teleport(game.PlaceId)
